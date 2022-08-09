2ab729789d97b9b26a0e77c1ed3961cf27016aeb
markmw <dant1kkz@gmail.com>
2022-08-09 14:11:09 +0600

    connect

---
+++ src/test/java/ru/job4j/pooh/TopicServiceTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.pooh;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TopicServiceTest {
+    @Test
+    public void whenTopic() {
+        TopicService topicService = new TopicService();
+        String paramForPublisher = "temperature=18";
+        String paramForSubscriber1 = "client407";
+        String paramForSubscriber2 = "client6565";
+        /* Режим topic. Подписываемся на топик weather. client407. */
+        topicService.process(
+                new Req("GET", "topic", "weather", paramForSubscriber1)
+        );
+        /* Режим topic. Добавляем данные в топик weather. */
+        topicService.process(
+                new Req("POST", "topic", "weather", paramForPublisher)
+        );
+        /* Режим topic. Забираем данные из индивидуальной очереди в топике weather. Очередь client407. */
+        Resp result1 = topicService.process(
+                new Req("GET", "topic", "weather", paramForSubscriber1)
+        );
+        /* Режим topic. Забираем данные из индивидуальной очереди в топике weather. Очередь client6565.
+        Очередь отсутствует, т.к. еще не был подписан - получит пустую строку */
+        Resp result2 = topicService.process(
+                new Req("GET", "topic", "weather", paramForSubscriber2)
+        );
+        assertThat(result1.getText()).isEqualTo("temperature=18");
+        assertThat(result2.getText()).isEqualTo("");
+    }
+}
