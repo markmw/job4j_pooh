bb06135bf8c18cec968e501b2f1f5024ef23ceee
markmw <dant1kkz@gmail.com>
2022-08-09 18:03:35 +0600

    2. Тестовое задание - проект "Pooh JMS" [#268841 #330402]

--- src/test/java/ru/job4j/pooh/ReqTest.java
+++ src/test/java/ru/job4j/pooh/ReqTest.java
@@ -11,8 +11,8 @@ public class ReqTest {
         String content = "POST /queue/weather HTTP/1.1" + ls
                 + "Host: localhost:9000" + ls
                 + "User-Agent: curl/7.72.0" + ls
-                + "Accept: */*" + ls +
-                "Content-Length: 14" + ls
+                + "Accept: */*" + ls
+                + "Content-Length: 14" + ls
                 + "Content-Type: application/x-www-form-urlencoded" + ls
                 + "" + ls
                 + "temperature=18" + ls;
